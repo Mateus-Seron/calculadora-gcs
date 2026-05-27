79feb02870d13b371341e026c5e7f8c22cb6679d
202319060496 <202319060496@lab03l>
2026-05-27 12:40:46 -0400

    fix: trata de elevar 0 a valor negativo e raiz quadrada de valor negativo

--- calc_potencia.py
+++ calc_potencia.py
@@ -4,10 +4,14 @@
 # Branch: feature/modulo-potencia
 
 def potencia(base, expoente):
+    if(base == 0 and expoente < 0):
+        return "Valores inválidos."
     return base ** expoente
 
 def raiz_quadrada(valor):
-    return valor * valor
+    if(valor < 0):
+        return "Valor negativo."
+    return valor ** (1/2)
 
 def raiz_cubica(valor):
-    return valor * valor * valor
+    return valor ** (1/3)
