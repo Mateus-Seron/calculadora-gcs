dece15717c2743dcbe6aa2b8df21838026c4244b
202319060496 <202319060496@lab03l>
2026-05-27 12:00:13 -0400

    feat: implementa operações de potência (potencialização, raiz quadrada, raiz cúbica)

---
+++ calc_potencia.py
@@ -0,0 +1,13 @@
+# calc_potencia.py
+# Módulo B — Operações de Potência
+# Autor: Mariana Paulino da Silva
+# Branch: feature/modulo-potencia
+
+def potencia(base, expoente):
+    return base ** expoente
+
+def raiz_quadrada(valor):
+    return valor * valor
+
+def raiz_cubica(valor):
+    return valor * valor * valor
